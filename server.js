fd318d154b1d2c6c4cb9523318d8757440610c50
dreamingmind <ddrake@dreamingmind.com>
2023-02-02 22:42:40 -0800

    add ‘/index’ to current ‘/‘ destination page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const {request} = require("express");
 
 // Constants
 const PORT = 8080;
@@ -8,8 +9,14 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.request.url
 app.get('/', (req, res) => {
-    res.send("Hello World ");
+    res.send("<h1>Hello World</h1><p> url: " + req.url + "</p> ");
+
+});
+
+app.get('/index', (req, res) => {
+    res.send("<h1>Brave New World </h1><p> url: " + req.url + "</p> ");
 
 });
 

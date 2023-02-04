b4f3cfdc5ef79ce4e00421ef9b2e93af69f7ff4e
dreamingmind <ddrake@dreamingmind.com>
2023-02-03 19:15:01 -0800

    simple class experiment

--- app.js
+++ app.js
@@ -5,3 +5,40 @@ console.log(logger);
 
 logger.log('message');
 
+let output = function() {
+    return 'this is the output';
+}
+
+module.exports.output = output;
+
+let x =-10;
+
+logger.log(-x);
+logger.log(x);
+
+const Method = function (input) {
+    this.input = input;
+    this.desc = `the value was ${input}`;
+}
+
+let Why = new Method('Why');
+let When = new Method('When');
+
+logger.log(Why.desc);
+logger.log(When.desc);
+logger.log(typeof Why);
+logger.log(typeof When);
+logger.log(typeof Method);
+
+const scanner = function(obj) {
+    logger.log(`\n========`);
+    for (prop in obj) {
+        logger.log(prop + ': ' + obj[prop]);
+    }
+    logger.log("========\n");
+}
+
+scanner(Why);
+scanner(When);
+
+
